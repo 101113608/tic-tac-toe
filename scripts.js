90a2be5c205019a8e90971c71aefe521121b8d93
101113608 <48575130+101113608@users.noreply.github.com>
2025-01-23 05:52:44 +1100

    Add 'Tile' factory function
    To hold the co-ordinates (row, column) of a specific tile on the
    tic-tac-toe board, and the current value that is on that tile
    (null or occupied with "X"/"O" marker), the 'Tile' factory function was
    added.

--- scripts.js
+++ scripts.js
@@ -7,3 +7,25 @@ function Player(playerName, playerMarker) {
 
     return { getMarker, getName }
 }
+
+function Tile(tileRow, tileCol) {
+    const row = tileRow;
+    const col = tileCol;
+
+    let value = null;
+
+    const getRow = () => row;
+    const getCol = () => col;
+
+    const setValue = (newValue) => {
+        if (value !== null) {
+            console.log(`Tile is already occupied: ${value}`);
+            return;
+        }
+        value = newValue;
+    }
+
+    const getValue = () => value;
+
+    return { getRow, getCol, setValue, getValue };
+}

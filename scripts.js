bce466ab1c962510a96ff2e8d6c7684f8e4f4328
101113608 <48575130+101113608@users.noreply.github.com>
2025-01-23 06:02:57 +1100

    Add 'GameBoard' factory function
    To create a 'tic-tac-toe' board - a 2D array, 3x3 in size that holds
    the tiles of the board, as well as methods pertaining to interacting
    with the board (placing markers, checking a specific tile on the board,
    etc.), the 'GameBoard' factory function was added.

--- scripts.js
+++ scripts.js
@@ -29,3 +29,57 @@ function Tile(tileRow, tileCol) {
 
     return { getRow, getCol, setValue, getValue };
 }
+
+function GameBoard() {
+    const rows = 3;
+    const cols = 3;
+    const board = [];
+
+    // Create 2D array, 3x3 size to represent the board
+    for (let i = 0; i < rows; i++) {
+        board[i] = [];
+        for (let j = 0; j < cols; j++) {
+            board[i][j] = Tile(i, j);
+        }
+    }
+
+    const getPosition = (row, col) => board[row][col];
+
+    const getEmptyPositions = () => {
+        let emptyPosition = [];
+        for (let i = 0; i < board.length; i++) {
+            for (let j = 0; j < board[i].length; j++) {
+                if (board[i][j].getValue() === null) {
+                    emptyPosition.push(board[i][j])
+                }
+            }
+        }
+        return emptyPosition;
+    }
+
+    const getBoard = () => board;
+
+    // For displaying board on console
+    const printBoard = () => {
+        let stringBoard = "";
+        for (let i = 0; i < rows; i++) {
+            stringBoard += "[";
+            for (let j = 0; j < cols; j++) {
+                stringBoard += " " + (board[i][j].getValue() !== null ? `[${board[i][j].getValue()}]` : "[ ]") + " ";
+            }
+            stringBoard += "]\n\n";
+        }
+        console.log(stringBoard);
+    }
+
+    const placeMarker = function (row, col, marker) {
+        if (board[row][col].getValue() !== null) {
+            console.log("Tile is already occupied");
+            return false;
+        }
+        board[row][col].setValue(marker);
+        return true;
+    }
+
+    return { getPosition, getBoard, getEmptyPositions, placeMarker, printBoard};
+}
